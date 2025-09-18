775a9a774bc90145e0a78429d03441e2ba5b94dd
Ratajje <amer.techn14@gmail.com>
2025-09-18 11:22:10 +0200

    Primera versión de mi repositorio de ejercicio

---
+++ src/MayorDeEdad.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class MayorDeEdad {
+    public static void main(String[] args) throws Exception {
+        String nombre;
+        int edad;
+        final int MAYORIA_DE_EDAD = 18;
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Escriba su nombre: ");
+        nombre = sc.nextLine(); 
+
+        System.out.println("Escriba su edad: ");
+        edad = sc.nextInt();
+
+        boolean mayorDeEdad = edad >= MAYORIA_DE_EDAD;
+
+        System.out.println("Hola, " + nombre);
+        System.out.println("Eres mayor de edad? " + mayorDeEdad);
+
+
+
+
+
+
+
+
+
+        /*
+         * Scanner sc = new Scanner(System.in);
+         * 
+         * System.out.println("Escriba su nombre");
+         * String n = sc.nextLine();
+         * 
+         * System.out.println("Escriba su edad");
+         * String e = sc.nextLine();
+         * 
+         * int mayorDeEdad;
+         * 
+         * mayorDeEdad = e >= 18;
+         * 
+         * System.out.println(n + " eres " + mayorDeEdad);
+         */
+
+    }
+
+}
