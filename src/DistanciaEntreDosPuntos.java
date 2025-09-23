86fdc84ff947eb33a7323758801b159cf47ebc87
amer <amer.techn14@gmail.com>
2025-09-23 14:29:23 +0200

    Realizo el ejercico de DistanciaEntreDosPuntos.java

--- src/DistanciaEntreDosPuntos.java
+++ src/DistanciaEntreDosPuntos.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class DistanciaEntreDosPuntos {
     public static void main(String[] args) throws Exception{
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Escriba el valor x1: ");
+        double x1 = sc.nextDouble();
+
+        System.out.print("Escriba el valor x2: ");
+        double x2 = sc.nextDouble();
         
+        System.out.print("Escriba el valor y1: ");
+        double y1 = sc.nextDouble();
+
+        
+        System.out.print("Escriba el valor y2: ");
+        double y2 = sc.nextDouble();
+
+        sc.close();
+
+        double distancia = Math.sqrt(Math.pow(x1, x2) + Math.pow(y1, y2));
+
+        System.out.print("Esta es la distancia entre dos puntos: " + distancia);
     }
 }
